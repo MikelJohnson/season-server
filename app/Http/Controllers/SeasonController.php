84a6bd321bffbcadb272b683a35dad4603371609
mj <mikel5764@gmail.com>
2024-05-05 15:52:40 +0300

    SeasonController image

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Season;
+use App\Models\SeasonImage;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class SeasonController extends Controller
 {
@@ -33,5 +37,41 @@ class SeasonController extends Controller
     {
         $data = $request->only(['description', 'season']);
         Season::create($data);
+    }
+
+    public function images(Request $request)
+    {
+        $query = SeasonImage::inRandomOrder();
+        if ($season = $request->get('season')) {
+            $query = $query->where('season', $season);
+        }
+        return response()->json($query->first(), options: JSON_UNESCAPED_SLASHES);
+    }
+
+    public function get_image(Request $request, int $id)
+    {
+        $image = SeasonImage::find($id);
+        $data = base64_decode($image->image);
+        return response($data)
+            ->header('Cache-Control', 'no-cache private')
+            ->header('Content-Description', 'File Transfer')
+            ->header('Content-Type', $image->mime_type)
+            ->header('Content-length', strlen($data))
+            ->header('Content-Transfer-Encoding', 'binary');
+    }
+
+    public function create_images()
+    {
+        return view('season_images_create');
+    }
+
+    public function store_images(Request $request)
+    {
+        $data = $request->only(['season']);
+        $path = $request->file('image_file')->getRealPath();
+        $data['filename'] = $request->file('image_file')->getClientOriginalName();
+        $data['mime_type'] = mime_content_type($path);
+        $data['image'] = base64_encode(file_get_contents($path));
+        SeasonImage::create($data);
     }
 }
