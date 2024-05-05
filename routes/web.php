73c018a81a3fce5a7e130d22321f20b0ee7f3049
mj <mikel5764@gmail.com>
2024-05-05 16:03:59 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SeasonController;
+use App\Http\Controllers\WeatherController;
+use Illuminate\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('seasons')->name('seasons.')->group(function (Router $router) {
+    Route::get('/descriptions', [SeasonController::class, 'descriptions'])->name('descriptions');
+    Route::get('/descriptions/create', [SeasonController::class, 'create_descriptions'])->name('create_descriptions');
+    Route::post('/descriptions', [SeasonController::class, 'store_descriptions'])->name('store_descriptions');
+    Route::get('/images', [SeasonController::class, 'images'])->name('images');
+    Route::get('/images/create', [SeasonController::class, 'create_images'])->name('create_images');
+    Route::post('/images', [SeasonController::class, 'store_images'])->name('store_images');
+    Route::get('/images/{id}', [SeasonController::class, 'get_image'])->name('get_image');
+});
+
+Route::get('/weather', [WeatherController::class, 'index'])->name('weather.index');
+Route::post('/weather', [WeatherController::class, 'store'])->name('weather.store');
