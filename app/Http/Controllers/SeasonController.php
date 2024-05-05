80ca90ec16892c0eaed29b7f717ba83c7fe1fc70
mj <mikel5764@gmail.com>
2024-05-05 15:24:30 +0300

    seasoncontroller descriptions

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class SeasonController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function descriptions(Request $request)
+    {
+        $query = Season::inRandomOrder();
+        if ($season = $request->get('season')) {
+            $query = $query->where('season', $season);
+        }
+        return response()->json($query->first());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create_descriptions()
+    {
+        return view('season_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store_descriptions(Request $request)
+    {
+        $data = $request->only(['description', 'season']);
+        Season::create($data);
+    }
 }
